c206ae541bf4e23f164931c941166018df08933d
Kudin <91375108+Alexander18101995@users.noreply.github.com>
2022-10-14 09:33:53 +0300

    Add files via upload

---
+++ app/views.py
@@ -0,0 +1,29 @@
+import os
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+from datetime import datetime
+
+def home_view(request):
+    template_name = 'app/home.html'
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+def time_view(request):
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+def workdir_view(request):
+    workdir = os.listdir()
+    return HttpResponse(workdir)
+
+
